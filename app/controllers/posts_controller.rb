dc6380235af92bfaad4d0116d4cc22c50c920c90
Jayon Kim <jpkim921@gmail.com>
2018-10-06 17:07:29 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,8 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.find(params[:id])
+		# raise params.inspect
+
 	end
 
 	def new
@@ -12,9 +14,10 @@ class PostsController < ApplicationController
 	end
 
 	def create
+		# binding.pry
 	  @post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
+	  @post.title = params[:post][:title]
+	  @post.description = params[:post][:description]
 	  @post.save
 	  redirect_to post_path(@post)
 	end
@@ -24,10 +27,18 @@ class PostsController < ApplicationController
 	end
 
 	def update
+		binding.pry
 	  @post = Post.find(params[:id])
 	  # @post.update(title: params[:title], description: params[:description])
     # we are updating the above line to below to use form_for
-    @post.update(params.require(:post))
+    # @post.update(params.require(:post))
+    @post.update(post_params)
 	  redirect_to post_path(@post)
+	end
+
+
+	private
+	def post_params
+		params.require(:post).permit(:title, :description)
 	end
 end
